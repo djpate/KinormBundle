9daf0e9f190a1de2d6cba4324a90b74b464f5e32
Christophe Verbinnen <pate@pepette.(none)>
2011-06-16 23:25:56 +0200

    config load

--- DependencyInjection/PateKinormExtension.php
+++ DependencyInjection/PateKinormExtension.php
@@ -3,11 +3,13 @@
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+
 class PateKinormExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('config.yaml');
     }
 
     public function getXsdValidationBasePath()
